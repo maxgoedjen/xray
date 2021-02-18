bdbc894b2ccc8168b55a565df7c7fbc3b66e6407
Max Goedjen <max.goedjen@gmail.com>
2021-02-17 19:00:25 -0800

    Project

---
+++ Xray/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Xray
+//
+//  Created by Max Goedjen on 2/17/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Xray/XrayApp.swift
@@ -0,0 +1,17 @@
+//
+//  XrayApp.swift
+//  Xray
+//
+//  Created by Max Goedjen on 2/17/21.
+//
+
+import SwiftUI
+
+@main
+struct XrayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
